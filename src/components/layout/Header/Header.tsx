98ba044c2ce55ab676a2a7e22699681ff48a9c6f
Lukaszglaz <glazzlukasz@gmail.com>
2024-05-01 01:41:37 +0200

    Added header components and styles for the header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,32 @@
+import { NavLink } from "react-router-dom";
+
 export const Header = () => {
   return (
     <>
-      <p>header</p>
+      <header className="header">
+        <div className="logo">
+          <h2>
+            <NavLink to="/">PL-Dezerter</NavLink>
+          </h2>
+        </div>
+
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="icons">
+          <i className="bx bx-menu-alt-right" id="menu-icon"></i>
+          <i className="bx bx-menu-alt-left" id="close-icon"></i>
+        </label>
+
+        <nav className="navbar">
+          <NavLink className="mn-active" to="/">
+            Strona Główna
+          </NavLink>
+          <NavLink to="/about-us">O Projekcie</NavLink>
+          <div className="container__btn_log">
+            <button className="login__btn">Zaloguj się</button>
+            <button className="register__btn">Zarejestruj się</button>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
