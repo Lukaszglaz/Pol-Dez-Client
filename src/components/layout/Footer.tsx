a6090fe81f63634dec00618386e09a38994b50c8
Lukaszglaz <glazzlukasz@gmail.com>
2024-05-01 12:35:14 +0200

    The footer layout and its styles have been created.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <>
-      <p>footer</p>
+      <footer className="footer">
+        <div className="contanier__contact_cc">
+          <h4>Informacje Kontaktowe</h4>
+          <p>
+            Adres e-mail:{" "}
+            <a href="mailto:kontakt@glazlukasz.pl">kontakt@glazlukasz.pl</a>
+          </p>
+        </div>
+        <div className="contanier__contact_cc">
+          <h4>Dla Klienta</h4>
+          <ul>
+            <li>
+              <Link to="/privacy-files-cookies">Polityka plików cookie</Link>
+            </li>
+            <li>
+              <Link to="/privacy-policy">Polityka prywatności</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="contanier__contact_cc">
+          <p>Copyright © 2024 - All Rights Reserved. By glazlukasz.pl</p>
+        </div>
+      </footer>
     </>
   );
 };
