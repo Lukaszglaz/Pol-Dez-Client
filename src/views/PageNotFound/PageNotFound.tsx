c1796cbcb81b7cb6814dc92f14899e1acf26bda7
Lukaszglaz <glazzlukasz@gmail.com>
2024-05-05 00:57:21 +0200

    Added its styles for the page not found view.

--- src/views/PageNotFound/PageNotFound.tsx
+++ src/views/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,10 @@
 export const PageNotFound = () => {
   return (
-    <>
-      <div>
-        <p>PageNotFound</p>
+    <div className="container_information_fnd">
+      <div className="error-message">
+        <h1>404</h1>
+        <p>Page Not Found</p>
       </div>
-    </>
+    </div>
   );
 };
