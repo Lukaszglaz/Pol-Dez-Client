86077fa151f9d492a0ff16f8c8aede735217d7f6
Lukaszglaz <glazzlukasz@gmail.com>
2024-06-21 08:27:07 +0200

    Added Tostify.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import { Router } from "./layout/Router";
 import { Footer } from "./layout/Footer";
 import { Header } from "./layout/Header/Header";
+import { ToastContainer } from "react-toastify";
 
 export const App = () => (
   <div className="container__app_main">
     <Header />
     <Router />
     <Footer />
+    <ToastContainer />
   </div>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App.tsx";
-import "./styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context.tsx";
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
