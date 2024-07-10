4cfffc7210799ec7cd5b1fd774d49e2285898d41
Lukaszglaz <glazzlukasz@gmail.com>
2024-07-11 00:37:59 +0200

    Naprawienie błędu protected routes.

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../contexts/user.context";
 
 export const ProtectedRoutes = () => {
-  const { user } = useUser();
+  const { user, isUserChecked } = useUser();
 
-  return user ? <Outlet /> : <Navigate to="/" />;
+  return isUserChecked ? user ? <Outlet /> : <Navigate to="/" /> : null;
 };
