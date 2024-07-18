dfcd648a1867f9137efb4bf5a3e699f8f1dcf44f
Lukaszglaz <glazzlukasz@gmail.com>
2024-07-18 21:23:54 +0200

    Added new windows to user stats profile.

--- src/views/Panel/PanelPlayer/PanelPlayer.tsx
+++ src/views/Panel/PanelPlayer/PanelPlayer.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 // import { useEffect } from "react";
 // import { callApi } from "../../../utils/api";
 // import { Method, PlayerResponse } from "../../../types";
 import Slider, { Settings } from "react-slick";
 import { usePanelInfo } from "../../../contexts/panel.context";
+import { useState } from "react";
 
 export const PanelPlayer = () => {
   const player = usePanelInfo().player;
@@ -25,6 +27,10 @@ export const PanelPlayer = () => {
     defenseWins,
     bestTrophies,
     bestBuilderBaseTrophies,
+    tag,
+    heroes,
+    troops,
+    spells,
   } = player;
 
   const settings: Settings = {
@@ -34,18 +40,113 @@ export const PanelPlayer = () => {
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
   };
+
+  const [activeTab, setActiveTab] = useState("Summary");
+
+  const handleTabClick = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+  const homeHeroes = heroes.filter((hero) => hero.village === "home");
+
+  const totalLevelHeroes = homeHeroes.reduce(
+    (sum, hero) => sum + hero.level,
+    0
+  );
+  const totalMaxLevelHeroes = homeHeroes.reduce(
+    (sum, hero) => sum + hero.hallMaxLevel,
+    0
+  );
+
+  const progressPercentageHeroes =
+    (totalLevelHeroes / totalMaxLevelHeroes) * 100;
+
+  // Builder Base Heroes
+
+  const homeBuilderHeroes = heroes.filter(
+    (hero) => hero.village === "builderBase"
+  );
+
+  const totalLevelBuilderHeroes = homeBuilderHeroes.reduce(
+    (sum, hero) => sum + hero.level,
+    0
+  );
+  const totalMaxLevelBuilderHeroes = homeBuilderHeroes.reduce(
+    (sum, hero) => sum + hero.hallMaxLevel,
+    0
+  );
+
+  const progressPercentageBuilderHeroes =
+    (totalLevelHeroes / totalMaxLevelBuilderHeroes) * 100;
+
+  // TROOPS STEP
+
+  const homeTroops = troops.filter((troops) => troops.village === "home");
+
+  const totalLevelTroops = homeTroops.reduce(
+    (sum, troops) => sum + troops.level,
+    0
+  );
+
+  console.log(totalLevelTroops);
+
+  const totalMaxLevelTroops = homeTroops.reduce(
+    (sum, troops) => sum + troops.hallMaxLevel,
+    0
+  );
+
+  const progressPercentageTroops =
+    (totalLevelTroops / totalMaxLevelTroops) * 100;
+
+  // Builder Base Troops
+
+  const homeBuilderTroops = troops.filter(
+    (troops) => troops.village === "builderBase"
+  );
+
+  const totalLevelBuilderTroops = homeBuilderTroops.reduce(
+    (sum, troops) => sum + troops.level,
+    0
+  );
+
+  console.log(totalLevelTroops);
+
+  const totalMaxLevelBuilderTroops = homeBuilderTroops.reduce(
+    (sum, troops) => sum + troops.hallMaxLevel,
+    0
+  );
+
+  const progressPercentageBuilderTroops =
+    (totalLevelBuilderTroops / totalMaxLevelBuilderTroops) * 100;
+
+  // SPELLS STEP
+
+  const homeSpells = spells.filter((spells) => spells.village === "home");
+
+  const totalLevelSpells = homeSpells.reduce(
+    (sum, spells) => sum + spells.level,
+    0
+  );
+  const totalMaxLevelSpells = homeSpells.reduce(
+    (sum, spells) => sum + spells.hallMaxLevel,
+    0
+  );
+
+  const progressPercentageSpells =
+    (totalLevelSpells / totalMaxLevelSpells) * 100;
+
   return (
     <main className="panel__content">
       <div className="profile-card">
         <div className="profile-card__picture">
-          <img
+          {/* <img
             src="/img/heroSkin.png"
             alt="Profile"
             className="profile-card__picture-img"
-          />
+          /> */}
         </div>
         <div className="profile-card__container">
           <div className="profile-card__name">
@@ -122,7 +223,7 @@ export const PanelPlayer = () => {
           <div className="profile-card__bests">
             <p className="profile-card__bests-label">Player's Tag</p>
             <div className="profile-card__best-container">
-              <div className="profile-card__best">#YCQ09GGG</div>
+              <div className="profile-card__best">{tag}</div>
             </div>
           </div>
         </div>
@@ -130,11 +231,176 @@ export const PanelPlayer = () => {
       <div className="profile-tabs">
         <div className="profile-tabs__link">
           <Slider {...settings}>
-            <div className="profile-tabs__link-list">Summary</div>
-            <div className="profile-tabs__link-list">Rankings</div>
-            <div className="profile-tabs__link-list">Army</div>
-            <div className="profile-tabs__link-list">Achievements</div>
+            <div
+              className={`profile-tabs__link-list ${
+                activeTab === "Summary" ? "active" : ""
+              }`}
+              onClick={() => handleTabClick("Summary")}
+            >
+              Summary
+            </div>
+            <div
+              className={`profile-tabs__link-list ${
+                activeTab === "Rankings" ? "active" : ""
+              }`}
+              onClick={() => handleTabClick("Rankings")}
+            >
+              Rankings
+            </div>
+            <div
+              className={`profile-tabs__link-list ${
+                activeTab === "Army" ? "active" : ""
+              }`}
+              onClick={() => handleTabClick("Army")}
+            >
+              Army
+            </div>
+            <div
+              className={`profile-tabs__link-list ${
+                activeTab === "Achievements" ? "active" : ""
+              }`}
+              onClick={() => handleTabClick("Achievements")}
+            >
+              Achievements
+            </div>
           </Slider>
+        </div>
+        <div className="profile-content">
+          {activeTab === "Summary" && (
+            <div className="profile-content__summary">
+              <div className="profile-content__summary-home">
+                <h2 className="profile-content__summary-main">Home Village</h2>
+                <div className="profile-content__summary-container">
+                  <div className="profile-content__summary-home__heroes">
+                    <p className="profile-content__summary-main__name">
+                      Heroes
+                    </p>
+                    {/* <h2 className="profile-content__summary-main"></h2> */}
+
+                    <div className="profile-content__summary-progressbar">
+                      <div
+                        className="profile-content__summary-progressbar__progress"
+                        style={{ width: `${progressPercentageHeroes}%` }}
+                      >
+                        <span className="profile-content__summary-main__lvl">
+                          {totalLevelHeroes}
+                        </span>
+                        /
+                        <span className="profile-content__summary-main__lvl">
+                          {totalMaxLevelHeroes}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-content__summary-home__heroes">
+                    <p className="profile-content__summary-main__name">
+                      Troops
+                    </p>
+                    {/* <h2 className="profile-content__summary-main"></h2> */}
+
+                    <div className="profile-content__summary-progressbar">
+                      <div
+                        className="profile-content__summary-progressbar__progress"
+                        style={{ width: `${progressPercentageTroops}%` }}
+                      >
+                        <span className="profile-content__summary-main__lvl">
+                          {totalLevelTroops}
+                        </span>
+                        /
+                        <span className="profile-content__summary-main__lvl">
+                          {totalMaxLevelTroops}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-content__summary-home__heroes">
+                    <p className="profile-content__summary-main__name">
+                      Spells
+                    </p>
+                    {/* <h2 className="profile-content__summary-main"></h2> */}
+
+                    <div className="profile-content__summary-progressbar">
+                      <div
+                        className="profile-content__summary-progressbar__progress"
+                        style={{ width: `${progressPercentageSpells}%` }}
+                      >
+                        <span className="profile-content__summary-main__lvl">
+                          {totalLevelSpells}
+                        </span>
+                        /
+                        <span className="profile-content__summary-main__lvl">
+                          {totalMaxLevelSpells}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-content__summary-home__levels"></div>
+                </div>
+              </div>
+              <div className="profile-content__summary-base">
+                <h2 className="profile-content__summary-main">Builder Base</h2>
+                <div className="profile-content__summary-container">
+                  <div className="profile-content__summary-home__heroes">
+                    <p className="profile-content__summary-main__name">
+                      Heroes
+                    </p>
+                    {/* <h2 className="profile-content__summary-main"></h2> */}
+
+                    <div className="profile-content__summary-progressbar">
+                      <div
+                        className="profile-content__summary-progressbar__progress"
+                        style={{ width: `${progressPercentageBuilderHeroes}%` }}
+                      >
+                        <span className="profile-content__summary-main__lvl">
+                          {totalLevelBuilderHeroes}
+                        </span>
+                        /
+                        <span className="profile-content__summary-main__lvl">
+                          {totalMaxLevelBuilderHeroes}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile-content__summary-home__heroes">
+                    <p className="profile-content__summary-main__name">
+                      Troops
+                    </p>
+                    {/* <h2 className="profile-content__summary-main"></h2> */}
+                    <div className="profile-content__summary-progressbar">
+                      <div
+                        className="profile-content__summary-progressbar__progress"
+                        style={{ width: `${progressPercentageBuilderTroops}%` }}
+                      >
+                        <span className="profile-content__summary-main__lvl">
+                          {totalLevelBuilderTroops}
+                        </span>
+                        /
+                        <span className="profile-content__summary-main__lvl">
+                          {totalMaxLevelBuilderTroops}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          {activeTab === "Rankings" && (
+            <div className="profile-content__rankings">
+              <p>Tutaj jest treść dla Rankings.</p>
+            </div>
+          )}
+          {activeTab === "Army" && (
+            <div className="profile-content__army">
+              <p>Tutaj jest treść dla Army.</p>
+            </div>
+          )}
+          {activeTab === "Achievements" && (
+            <div className="profile-content__achievements">
+              {/* Treść dla Achievements */}
+              <p>Tutaj jest treść dla Achievements.</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
